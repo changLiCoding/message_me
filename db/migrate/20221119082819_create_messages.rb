b3cad445ab415056750b6b61e6a6e97639d8e419
javascriptsucks <evix1988@live.cn>
2022-11-22 01:11:36 +0800

    fix intseger bug

--- db/migrate/20221119082819_create_messages.rb
+++ db/migrate/20221119082819_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.string :body
-      t.intseger :user_id
+      t.integer :user_id
       t.timestamps
     end
   end
